6817a474bae68cddb5cab492eca7267c728a7207
Amol Udage <amol.udage@pattern.com>
2023-12-15 12:43:34 +0530

    Send payload tags only when present

--- lib/datadog_notifier.rb
+++ lib/datadog_notifier.rb
@@ -4,7 +4,7 @@ class DatadogNotifier
   def self.notify(exception, payload = {})
     root_span = find_root_span(Datadog::Tracing.active_span)
     root_span.set_error(exception)
-    root_span.set_tag('payload', payload.to_json)
+    root_span.set_tag('payload', payload.to_json) if payload.present?
   end
 
   def self.find_root_span(child_span)
